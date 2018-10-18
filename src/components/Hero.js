5b78d1a01fae7633c80f3168689386a1dd5e4e9d
Mark Dino <markdinopelonia447@gmail.com>
2018-10-18 19:07:55 +0800

    feat: Hero components with cover Title and subtitle

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Jumbotron } from "reactstrap";
+import "../App.css";
+const Hero = () => (
+  <div>
+    <main className="cover-page">
+      <Jumbotron className="text-center">
+        <h1>Manila Tourism</h1>
+        <h3>Tours | Travel | Guide</h3>
+      </Jumbotron>
+    </main>
+  </div>
+);
+
+export default Hero;
