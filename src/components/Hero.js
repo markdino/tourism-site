e2b96f214f42ee3e1dae28188545ab4ba958deaa
Mark Dino <markdinopelonia447@gmail.com>
2018-10-18 23:21:09 +0800

    fix: wrapped & centered - Title, Subtitle, & button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Jumbotron } from "reactstrap";
+import { Button } from "reactstrap";
 import "../App.css";
 const Hero = () => (
   <div>
     <main className="cover-page" id="hero">
-      <Jumbotron className="text-center">
-        <h1>Manila Tourism</h1>
-        <h3>Tours | Travel | Guide</h3>
-      </Jumbotron>
+      <section className="wrapped-page">
+        <div className="item-center">
+          <h1>Manila Tourism</h1>
+          <h3>Tours | Travel | Guide</h3>
+          <Button outline color="warning">
+            Explore More
+          </Button>{" "}
+        </div>
+      </section>
     </main>
   </div>
 );
