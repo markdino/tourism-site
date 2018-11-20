089af3b9e3403ac9f83929dec59ca2e2c4016540
Mark Dino <markdinopelonia447@gmail.com>
2018-11-20 23:08:45 +0800

    Add tour cards

--- src/components/Package.js
+++ src/components/Package.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText
+} from "reactstrap";
 import "../App.css";
+import beach from "../img/beach.jpg";
+import resort from "../img/resort.jpg";
+import camp from "../img/camp.jpg";
+import mountain from "../img/mountain.jpg";
+import gulp from "../img/gulp.jpg";
 
 class Package extends React.Component {
   render() {
@@ -11,6 +26,80 @@ class Package extends React.Component {
             <h1>Tour Packages</h1>
             <p>A Great Collection of Our Tour Packages</p>
           </header>
+          <section>
+            <Row>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src={beach} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src={gulp} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src={resort} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src={mountain}
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col sm="4">
+                <Card>
+                  <CardImg top width="100%" src={camp} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </section>
         </Container>
       </div>
     );
